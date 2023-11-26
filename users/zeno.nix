32735d478bd315292e8fe0f2cc6002d85cdd9349
zeno <zacheryklinglesmith@gmail.com>
2023-11-26 17:16:44 -0500

    Fix indentation mistake

--- users/zeno.nix
+++ users/zeno.nix
@@ -9,38 +9,39 @@
         home-manager = {
           useGlobalPkgs = true;
           useUserPackages = true;
-            users.zeno = {
-              home = {
-                stateVersion = "23.05";
-              };
-              imports = [
-                ../programs/neovim.nix
-                ../programs/kakoune.nix
-                ../programs/kitty.nix
-                ../programs/i3.nix
-              ];
+          users.zeno = {
+            home = {
+              stateVersion = "23.05";
+            };
+            imports = [
+              ../programs/neovim.nix
+              ../programs/kakoune.nix
+              ../programs/kitty.nix
+              ../programs/i3.nix
+            ];
 
-              programs = {
-                home-manager.enable = true;
-                firefox = {
-                  enable = true;
-                  profiles.default.settings = {
-                    "browser.download.dir" = "/home/zeno/documents/Downloads";
-                  };
-                };
-
-                git = {
-                  enable = true;
-                  userName = "zeno";
-                  userEmail = "zacheryklinglesmith@gmail.com";
-                  extraConfig = {
-                    core = {
-	              editor = "nvim";
-	            };
-                  };
+            programs = {
+              home-manager.enable = true;
+              firefox = {
+                enable = true;
+                profiles.default.settings = {
+                  "browser.download.dir" = "/home/zeno/documents/Downloads";
                 };
               };
-           };
-        };
-}];
+
+              git = {
+                enable = true;
+                userName = "zeno";
+                userEmail = "zacheryklinglesmith@gmail.com";
+                extraConfig = {
+                  core = {
+	            editor = "nvim";
+	          };
+                };
+              };
+            };
+         };
+      };
+    }
+  ];
 }
